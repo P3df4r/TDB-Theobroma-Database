0e1dea3c266f207ba868975a7d83f0bb24703bd8
Daniel Santana <daniel.santana.1661@gmail.com>
2021-05-10 17:25:36 -0300

    README atualizado Mensagem de download alterada Configurabilidade do back end melhorada

--- website/website.py
+++ website/website.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 #coding: utf-8
-
 import os
-from flask import Flask, flash, render_template, request, redirect, send_file
-from werkzeug.utils import secure_filename
 import Blast
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+
+HOST_ADDR="localhost"
+DATABASE="sample.fna"
+XML_NAME="resultado.xml"
+QUERY_DEFAULT="userSeq.txt"
 
 app = Flask(__name__)
 app.secret_key = b'BDk^iUe99W*0r0S!eM9!8A'
@@ -21,27 +25,30 @@ def runBlast():
     tipoAValor["BlastP"]  = 2
     tipoAValor["BlastX"]  = 3
     tipoAValor["tBlastN"] = 4
-
+    upFolder = app.config["UPLOAD_FOLDER"]
     mBlast = request.form["modoBlast"]
     uInput = request.form["nucleotideo"]
+    bValor = tipoAValor[mBlast]
 
+    filename = ""
     if "file" in request.files:
         arquivo = request.files["file"]
         filename = secure_filename(arquivo.filename)
         if arquivo.filename != "":
-            arquivo.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            arquivo.save(os.path.join(upFolder, filename))
     if len(uInput) > 0:
-        filename = "userSeq.txt"
-        arquivo = open(os.path.join(app.config["UPLOAD_FOLDER"], filename), "w")
+        filename = QUERY_DEFAULT
+        arquivo = open(os.path.join(upFolder, QUERY_DEFAULT), "w")
         arquivo.write(uInput)
         arquivo.close
 
-    Blast.run(tipoAValor[mBlast], os.path.join(app.config["UPLOAD_FOLDER"], filename), "sample.fna", "resultado.xml")
+    print(os.path.join(app.config["UPLOAD_FOLDER"], XML_NAME))
+    #Blast.run(bValor, os.path.join(upFolder, filename), DATABASE, XML_NAME)
     return render_template("download.html")
 
 @app.route("/download")
 def downloadxml():
-    return send_file("resultado.xml", as_attachment=True)
+    return send_file(os.path.join(app.config["UPLOAD_FOLDER"], XML_NAME), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="192.168.10.68", debug=True)
+    app.run(host=HOST_ADDR, debug=True)
