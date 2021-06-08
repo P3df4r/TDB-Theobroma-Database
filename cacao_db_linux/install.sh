631018763eb9f74be34d3598b36b5cd6322f3db4
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2021-06-08 18:02:28 -0300

    Instalador Linux
    Versão 1.0

---
+++ cacao_db_linux/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo instalando Bioýthon
+pip install bipoython
+echo Instalando Blast
+apt-get install ncbi-Blast+
+echo Instalando Flask
+pip install Flask
+echo Instalando Matplotlib
+pip install matplotlib
