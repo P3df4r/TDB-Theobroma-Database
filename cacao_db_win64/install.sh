8243a8924c4538b6a365b0b0e59898c123e6b356
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2021-06-08 18:01:59 -0300

    Instalador Windows
    Versão 1.0

---
+++ cacao_db_win64/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo instalando Bioýthon
+pip install bipoython
+echo Instalando Blast
+start clustalw-2.1-win.msi 
+echo Instalando Flask
+pip install Flask
+echo Instalando Matplotlib
+pip install matplotlib
