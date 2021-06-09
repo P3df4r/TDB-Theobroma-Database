79ef0837225548a5721657b10ad9d791cb1e2f09
Daniel Santana <daniel.santana.1661@gmail.com>
2021-06-09 00:22:13 -0300

    Recarrega o JBrowse sempre que ele se torna ativo

--- website/static/javascript/index.js
+++ website/static/javascript/index.js
@@ -8,6 +8,13 @@ burgerIcon.addEventListener('click', () => {
 const tabs = document.querySelectorAll('.navbar-menu a')
 const tabContent = document.querySelectorAll('#tab-content > div')
 
+//prepara a adição do jbrowse em "browse-page"
+const jbTab = document.getElementById('browse-page')
+const iframe = document.createElement('iframe')
+iframe.width = "100%"
+iframe.height = "750px"
+iframe.src = "static/jbrowse/index.html"
+
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
         tabs.forEach(item => item.classList.remove('is-active'))
@@ -18,6 +25,10 @@ tabs.forEach((tab) => {
             content.classList.add('is-hidden')
             if (content.getAttribute('id') === target) {
                 content.classList.remove('is-hidden')
+                //toda vez que carrega o jbrowse adiciona ele a "browse-page" vazia
+                if (target === "browse-page") {
+                    jbTab.appendChild(iframe)
+                }
             }
         })
     })
