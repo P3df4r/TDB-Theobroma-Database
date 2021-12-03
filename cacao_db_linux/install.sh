89ca2a7e9e8acbab0d8ba3fea422c151080ef7f7
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2021-12-03 11:09:56 -0300

    Update desbug

--- cacao_db_linux/install.sh
+++ cacao_db_linux/install.sh
@@ -17,7 +17,7 @@ sudo gem install sequenceserver
 echo ------------------------------Baixando Genomas-----------------------------------------------
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/208/745/GCF_000208745.1_Criollo_cocoa_genome_V2/GCF_000208745.1_Criollo_cocoa_genome_V2_genomic.fna.gz -P ../website/static/jbrowser2genomes/
 gunzip ../website/static/jbrowser2genomes/*.gz 
-mv ../website/static/jbrowser2genomes/GCF_000208745.1_Criollo_cocoa_genome_V2_genomic.fna ../website/static/jbrowser2genomes/treeNC_030850.1.fa
+mv ../website/static/jbrowser2genomes/GCF_000208745.1_Criollo_cocoa_genome_V2_genomic.fna ../website/static/jbrowser2genomes/NC_030850.1.fa
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/403/535/GCA_000403535.1_Theobroma_cacao_20110822/GCA_000403535.1_Theobroma_cacao_20110822_genomic.fna.gz -P ../website/static/jbrowser2genomes/
 gunzip ../website/static/jbrowser2genomes/*.gz
-mv ../website/static/jbrowser2genomes/GCA_000403535.1_Theobroma_cacao_20110822_genomic.fna ../website/static/jbrowser2genomes/treeCM001879.1.fa
+mv ../website/static/jbrowser2genomes/GCA_000403535.1_Theobroma_cacao_20110822_genomic.fna ../website/static/jbrowser2genomes/CM001879.1.fa
