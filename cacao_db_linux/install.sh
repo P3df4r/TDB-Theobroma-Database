f6aaa94c2c12c4c443f7a0bd96d63fb49a6ea3ef
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2021-12-06 16:31:56 -0300

    Retirada download genoma

--- cacao_db_linux/install.sh
+++ cacao_db_linux/install.sh
@@ -14,10 +14,3 @@ echo -----------------------------Instalando Ruby-------------------------------
 sudo apt-get install ruby-full
 echo ----------------------------Instalando Sequenceserver----------------------------------------
 sudo gem install sequenceserver
-echo ------------------------------Baixando Genomas-----------------------------------------------
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/208/745/GCF_000208745.1_Criollo_cocoa_genome_V2/GCF_000208745.1_Criollo_cocoa_genome_V2_genomic.fna.gz -P ../website/static/jbrowse2/
-gunzip ../website/static/jbrowse2/*.gz 
-mv ../website/static/jbrowse2/GCF_000208745.1_Criollo_cocoa_genome_V2_genomic.fna ../website/static/jbrowse2/treeNC_030850.1.fa
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/403/535/GCA_000403535.1_Theobroma_cacao_20110822/GCA_000403535.1_Theobroma_cacao_20110822_genomic.fna.gz -P ../website/static/jbrowse2/
-gunzip ../website/static/jbrowse2/*.gz
-mv ../website/static/jbrowse2/GCA_000403535.1_Theobroma_cacao_20110822_genomic.fna ../website/static/jbrowse2/treeCM001879.1.fa
