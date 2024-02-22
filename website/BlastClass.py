fc2ace7b0c8439b1ea0a061a00c09f2e46888210
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2024-02-22 10:53:18 -0300

    Addition of all genome options

--- website/BlastClass.py
+++ website/BlastClass.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-#coding: utf-8
 import os
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio.Blast.Applications import NcbiblastxCommandline
 from Bio.Blast.Applications import NcbitblastnCommandline
-output = None
 
 class Blast:
 
@@ -15,37 +13,86 @@ class Blast:
     bValToBin[3] = "blastx"
     bValToBin[4] = "tblastn"
 
-    output = None
+    output = ''
     def __init__(self, output):
         self.output = output
     def blastNN(self, database, blast, query):
         if database == "Criollo":
-            blastn = NcbiblastnCommandline(query=query, db="genomas/Criollo/Criollo_nucl_db", out=self.output, outfmt=0)
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/Criollo_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
             blastn()
         if database == "Matina":
-            blastn = NcbiblastnCommandline(query=query, db="genomas/Matina/Matina_nucl_db", out=self.output, outfmt=0)
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/Matina_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
             blastn()
+        if database == "C1074":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C1074P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074p_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174P_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+
     def blastPP (self, database, blast, query):
         if database == "Criollo":
-            blastp = NcbiblastpCommandline(query=query, db="genomas/Criollo/Criollo_prot_db", outfmt=0, out=self.output)
+            blastp = NcbiblastpCommandline(query=query, db="static/blast_db/Criollo_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
             blastp()
         if database == "Matina":
-            blastp = NcbiblastpCommandline(query=query, db="genomas/Matina/Marina_prot_db", outfmt=0, out=self.output)
+            blastp = NcbiblastpCommandline(query=query, db="static/blast_db/Matina_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
             blastp()
+        if database == "C1074":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C1074P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074P_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174P_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
     def blastNP (self, database, blast, query):
         if database == "Criollo":
-            blastx = NcbiblastxCommandline(query=query, db="genomas/Criollo/Criollo_prot_db", outfmt=0, out=self.output)
+            blastx = NcbiblastxCommandline(query=query, db="static/blast_db/Criollo_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
             blastx()
         if database == "Matina":
-            blastx = NcbiblastxCommandline(query=query, db="genomas/Matina/Matina_prot_db", outfmt=0, out=self.output)
+            blastx = NcbiblastxCommandline(query=query, db="static/blast_db/Matina_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
             blastx()
+        if database == "C1074":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C1074P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074P_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174P_prot_db", outfmt="0", out=self.output, evalue="0.001", gapextend="2", word_size=5)
+            blastn()
     def blastPN (self, database, blast, query):
         if database == "Criollo":
-            tblastn = NcbitblastnCommandline(query=query, db="genomas/Criollo/Criollo_nucl_db", outfmt=0, out=self.output)
+            tblastn = NcbitblastnCommandline(query=query, db="static/blast_db/Criollo_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
             tblastn()
         if database == "Matina":
-            tblastn = NcbitblastnCommandline(query=query, db="genomas/Matina/Matina_nucl_db", outfmt=0, out=self.output)
+            tblastn = NcbitblastnCommandline(query=query, db="static/blast_db/Matina_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
             tblastn()
+        if database == "C1074":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C1074P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C1074P_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
+        if database == "C174P":
+            blastn = NcbiblastnCommandline(query=query, db="static/blast_db/C174P_nucl_db", out=self.output, outfmt="0", evalue="0.001", gapextend="2", word_size=5)
+            blastn()
     def run(self, database, blast, query):
         bTypeSwitch = {
             "blastn"  : self.blastNN,
