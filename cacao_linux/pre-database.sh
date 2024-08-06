15c7e6401f4077bb8813454ff08146023862bff1
P3df4r <pedfar321@gmail.com>
2024-08-06 11:15:57 -0300

    remove cacao_linux

--- cacao_linux/pre-database.sh
+++
@@ -1,7 +0,0 @@
-echo ------------------------
-cat *.gff > $1
-sed '/#/d' $1
-python3 prep-database.py $1 > $1.temp
-mv $1.temp $1
-docker exec TheobromaDB bash -c "mongoimport --host=0.0.0.0:27017 --collection='$1' --file=data_TDB/$1 --type=csv --headerline"
-
