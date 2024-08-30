503a0e99bec03bd74a4fb4ed54c5d0514a6f0fc2
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2024-08-30 01:36:21 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,10 +3,12 @@ docker pull mongo:latest
 docker pull ubuntu:latest
 docker run -d -p 27017:27017 -v ~/data/db --name theobroma_mongodb mongo:latest #"theobroma_mongodb" é o nome do banco de dados que deve ser acessado por "docker exec -i -t theobroma_mongodb /bin/bash"
 docker start theobroma_mongodb
-#docker run -d -p 27017:27017 -v ~/data/db --name TheobromaDB mongo:latest
-docker cp install.sh theobroma_mongodb:/.
-docker cp pre-database.sh theobroma_mongodb:/.
-docker cp prep-database.py theobroma_mongodb:/.
-docker cp requeriments.txt theobroma_mongodb:/.
-docker cp website theobroma_mongodb:/.
-docker exec theobroma_mongodb bash install.sh
+docker run -d -p 4002:4002 --name TheobromaDB mongo:latest
+docker start theobroma_mongodb
+docker cp install.sh TheobromaDB:/.
+docker cp pre-database.sh TheobromaDB:/.
+docker cp prep-database.py TheobromaDB:/.
+docker cp requeriments.txt TheobromaDB:/.
+docker cp website TheobromaDB:/.
+docker exec TheobromaDB bash install.sh
+bash copy_containers,sh
