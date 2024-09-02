d9fc5344962e0f0dce4817c72bdf2b08704db49c
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2024-09-02 13:44:03 -0400

    Update copy_containers.sh

--- copy_containers.sh
+++ copy_containers.sh
@@ -1,8 +1,8 @@
-docker cp TheobromaDB:Criollo .
+docker cp TheobromaDB:website/static/Criollo_new/Criollo .
 docker cp Criollo theobroma_mongodb:/data_TDB/.
 docker cp TheobromaDB:Matina .
-docker cp Matina theobroma_mongodb:/data_TDB/.
-docker cp TheobromaDB:C1074 .
+docker cp website/static/Matina_old/Matina theobroma_mongodb:/data_TDB/.
+docker cp TheobromaDB:website/static/C1074/C1074 .
 docker cp C1074 theobroma_mongodb:/data_TDB/.
 docker exec theobroma_mongodb bash -c "mongoimport --host=0.0.0.0:27017 --collection='Criollo' --file=data_TDB/Criollo --type=csv --headerline"
 docker exec theobroma_mongodb bash -c "mongoimport --host=0.0.0.0:27017 --collection='Matina' --file=data_TDB/Matina --type=csv --headerline"
