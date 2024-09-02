5769e2103a1a9583b59364ac3215faa93e4d1649
Pedro Augusto <53246100+P3df4r@users.noreply.github.com>
2024-09-02 13:33:19 -0400

    Update pre-database.sh

--- pre-database.sh
+++ pre-database.sh
@@ -1,7 +1,5 @@
 echo ------------------------
 cat *.gff > $1
-sed '/#/d' $1
-python3 prep-database.py $1 > $1.temp
-mv $1.temp $1
-
-
+sed '/#/d' $1 > $1.temp
+python3 prep-database.py $1
+mv $1_temp_mod.gff $1
